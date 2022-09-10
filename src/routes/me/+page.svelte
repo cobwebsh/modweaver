efaec2324e21e9d299e180022551a81986a8d201
ExoPlant <lennyj2468+github@gmail.com>
2022-09-10 11:06:14 +0100

    add avatar uploading

--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -12,6 +12,9 @@
 	let isLoading = true;
 
 	let profile: Profile;
+
+	let files: FileList | null | undefined;
+
 	onMount(async () => {
 		// force cast to Profile because we can be certain that it will exist
 		profile = (await supabase
@@ -41,7 +44,38 @@
 
 	async function saveProfileChanges() {
 		isLoading = true;
-
+		if (
+			profile?.avatar_url?.indexOf('http') != -1 &&
+			profile?.avatar_url?.indexOf(':') != -1 &&
+			profile?.avatar_url?.indexOf('//') != -1
+		) {
+		} else {
+			var err = 'Invalid avatar url!';
+			console.error(err);
+			alert(err);
+			window.location.reload();
+			return;
+		}
+		if (
+			profile?.website_url?.indexOf('http') != -1 &&
+			profile?.website_url?.indexOf(':') != -1 &&
+			profile?.website_url?.indexOf('//') != -1
+		) {
+		} else {
+			var err = 'Invalid website url!';
+			console.error(err);
+			alert(err);
+			window.location.reload();
+			return;
+		}
+		let url = await await supabase.storage
+			.from('avatars')
+			.getPublicUrl(profile.id + '.' + (await changeAvatar(files))).publicURL;
+		if (url != null) {
+			profile.avatar_url = url;
+		} else {
+			console.error('URL was null! Please try again');
+		}
 		const { error } = await supabase
 			.from('profiles')
 			.update({
@@ -56,18 +90,47 @@
 
 		isLoading = false;
 	}
-    async function changeAvatar(file: File) {
-        const validExts = ["png", "jpg", "gif"];
-        const fileExt = file.name.split(".")[file.name.split(".").length];
-        if (validExts.includes(fileExt)) {
-            await supabase.from("avatars").remove([profile.id])
-        } else {
-            // TODO: replace with own error models
-            var error = "Invalid image extension: " + fileExt;
-            console.error(error);
-            alert(error);   
-        }
-    }
+	async function changeAvatar(file: FileList | null | undefined): Promise<string | undefined> {
+		const validExts = ['png', 'jpg', 'gif'];
+		if (file != null && file != undefined) {
+			for (var i = 0; file.length > i; i++) {
+				let f = file.item(i);
+				if (f != null) {
+					const fileExt = f.name.split('.')[f.name.split('.').length - 1];
+					console.log(f.name + ' : ' + f.name.split('.').length);
+					if (validExts.includes(fileExt)) {
+                        let name = profile.id + "." + fileExt;
+                        console.log("hfgfdrh : " + name)
+						/*let { error } = await supabase.storage.from('avatars').remove([name]);
+                        let error_remove = error;
+                        if (error_remove) {
+                            console.error(error_remove)
+                        }
+						({error} = (await supabase.storage.from('avatars').upload(name, f)));
+                        let error_upload = error;
+                        if (error_upload) {
+                            console.error(error_upload)
+                        }*/
+                        await supabase.storage.from('avatars').update(name, f, {
+                            upsert: true
+                        })
+					} else {
+						// TODO: replace with own error models
+						var error = 'Invalid image extension: ' + fileExt;
+						console.error(error);
+						alert(error);
+					}
+					if (i == file.length) {
+						console.log(fileExt + ' : fileExt');
+						return fileExt;
+					} else {
+                        console.log("Beep : " + i + " : " + file.length)
+                    }
+				}
+			}
+		}
+        return "png";
+	}
 </script>
 
 {#if isLoading}
@@ -86,13 +149,14 @@
 		<input id="website" type="text" bind:value={profile.website_url} />
 
 		<!-- TODO: Add avatar uploading -->
-		{#if profile.avatar_url}
+		{#if profile.avatar_url && profile.avatar_url?.indexOf('http') != -1}
 			<img class="avatar" alt="Your avatar" src={profile.avatar_url} />
 		{:else}
 			<div class="blank avatar">No avatar</div>
 		{/if}
-		<label for="avatar">Avatar URL</label>
-		<input id="avatarUrl" type="text" bind:value={profile.avatar_url} />
+		<label for="avatar">Avatar</label>
+		<input type="file" id="avatar" bind:files />
+		
 
 		<button type="submit">Save</button>
 	</form>
