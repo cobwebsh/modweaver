c53e47e93f2c9031b6244a769423b70985aa9e51
ExoPlant <lennyj2468+github@gmail.com>
2022-10-15 11:29:07 +0100

    h

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,11 +1,11 @@
 /** Publicly available information about a user */
 export interface Profile {
 	/** This is the same UUID as the associated user */
-	id?: string;
+	id: string;
 	/** A display name */
-	username?: string;
+	username: string;
 	/** A URL pointing to the user's avatar image */
-	avatar_url?: string;
+	avatar_url: string;
 	/** A URL pointing to the user's website */
-	website_url?: string;
+	website_url: string;
 }
